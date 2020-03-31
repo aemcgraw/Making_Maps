b8f988284a7d165149e281e8e61eb2f32f5d15c9
Andrew McGraw <mcgraw4@illinois.edu>
2020-03-30 20:36:12 -0700

    Remove old files

--- util.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-import math
-
-def get_scaled_log(maximum):
-    input = float(randint(1, maximum))
-    initial_value = math.log(maximum / input, 10)
-    scaling_factor = maximum / math.log(maximum, 10)
-    temp = math.floor(initial_value * scaling_factor)
-    return temp
